3829b5ccbd30778808d6ea153cb0d3cf7f15cf73
Volodymyr-SH-2005 <vova.sh2005@gmail.com>
2025-07-11 17:18:42 +0300

    hw-les01 done

---
+++ hw/hw-les01/main.js
@@ -0,0 +1,37 @@
+//1
+let a01 = 'hello';
+let a02 = 'owu';
+let a03 = 'com';
+let a05 = 'ua';
+let a06 = 1;
+let a07 = 10;
+let a08 = -999;
+let a09 = 123;
+let a10 = 3.14;
+let a11 = 2.7;
+let a12 = 16;
+let a13 = true;
+let a14 = false;
+
+console.log(a01);
+console.log(a02);
+console.log(a03);
+console.log(a05);
+console.log(a06);
+console.log(a07);
+console.log(a08);
+console.log(a09);
+console.log(a10);
+console.log(a11);
+console.log(a12);
+console.log(a13);
+console.log(a14);
+
+//4
+let userName = prompt("Будь ласка, введіть ваше ім'я:");
+if (userName) {
+    console.log("Привіт, " + userName + "!");
+} else {
+    console.log("Ви не ввели ім'я");
+}
+
